d3e4fbcd5388157f238ec31ef017d8c511939f60
JIAN JIAN <ukgang@hotmail.com>
2019-11-03 14:34:33 -0500

    Change data source of airline survey

--- report/Project_Code.R
+++ report/Project_Code.R
@@ -169,8 +169,8 @@ na.handler <- function(ds, strategy) {
 loginfo("Function initialization complete! ", logger="")
 
 # 1. Read data from dataset
-dsPath = "/Users/ukgang/Desktop/github/IST-687/dataset/fall2019-survey-M04.json";
-rawData <- jsonlite::read_json(path = dsPath, simplifyVector = T);
+dsUrl = "https://raw.githubusercontent.com/UKGANG/IST-687/master/dataset/fall2019-survey-M04.json";
+rawData <- jsonlite::fromJSON(getURL(dsUrl));
 loginfo(sprintf("JSON data loaded, row=%d, column=%d. "
                 , dim(rawData)[1], dim(rawData)[2]), logger="");
 
