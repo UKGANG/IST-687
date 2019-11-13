de41ba1da69a663aed6fa788cf6e21eec77ff7b9
JIAN JIAN <ukgang@hotmail.com>
2019-11-13 01:57:36 -0500

    Flight route visualization

---
+++ analysis/visualization/Airline_Route_Render.R
@@ -0,0 +1,10 @@
+source("https://raw.githubusercontent.com/UKGANG/IST-687/master/analysis/dataSnooping/Map_Render.R")
+
+flightRouteRender <- function() {
+  return (geom_curve(data = airlineRoute, aes(x = Geom.Departure.Airport.Longitude, xend = Geom.Arrival.Airport.Longitude
+                                              ,y = Geom.Departure.Airport.Latitude,  yend = Geom.Arrival.Airport.Latitude)
+                     ,alpha=0.25, size=0.1, color = "white", inherit.aes = FALSE))
+}
+
+
+renderFlightInformation(flightRouteRender)
