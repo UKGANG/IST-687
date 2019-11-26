251d55c21ed5b7a2f15a378a87a5e5eb70bb4518
JIAN JIAN <ukgang@hotmail.com>
2019-11-26 02:02:20 -0500

    Map visualization revise

--- analysis/visualization/Map_Render.R
+++ analysis/visualization/Map_Render.R
@@ -2,11 +2,10 @@ source("https://raw.githubusercontent.com/UKGANG/IST-687/master/analysis/visuali
 source("https://raw.githubusercontent.com/UKGANG/IST-687/master/analysis/visualization/Airline_Route_Module.R")
 source("https://raw.githubusercontent.com/UKGANG/IST-687/master/analysis/visualization/City_Frequency_Module.R")
 
+# Map render configuration
 map.render.zoom.x=c(-160,-65)
 map.render.zoom.y=c(14,64)
 map.render.alpha = 0.19
 map.render.size = 1.7 
-
-renderFlightInformation(c(flightRouteRender, cityFrequencyRender))
 
 renderFlightInformation(c(flightRouteRender, cityFrequencyRender))
