375a37adaf94d9b1a02d6e4548332d23ae297638
JIAN JIAN <ukgang@hotmail.com>
2019-12-02 22:44:38 -0500

    Add sunday dummy

--- mungling/dummy_builder.R
+++ mungling/dummy_builder.R
@@ -8,7 +8,7 @@ rawData$weekDay <- wday(strptime(rawData$Flight.Date, "%m/%d/%y"));
 rawData <- arrange(rawData, desc(rawData$month))
 rawData <- dummy_columns(rawData, "month", remove_first_dummy = T)
 rawData <- arrange(rawData, desc(rawData$weekDay))
-rawData <- dummy_columns(rawData, "weekDay", remove_first_dummy = T)
+rawData <- dummy_columns(rawData, "weekDay")
 
 rawData$January <- rawData[,"month_1"]
 rawData$February <- rawData[,"month_2"]
@@ -18,9 +18,11 @@ rawData$Wednesday <- rawData[,"weekDay_3"]
 rawData$Thursday <- rawData[,"weekDay_4"]
 rawData$Friday <- rawData[,"weekDay_5"]
 rawData$Saturday <- rawData[,"weekDay_6"]
+rawData$Sunday <- rawData[,"weekDay_7"]
 
 rawData <- select(rawData, -c("month_1", "month_2"
                               , "weekDay_1", "weekDay_2"
                               , "weekDay_3", "weekDay_4"
-                              , "weekDay_5", "weekDay_6"))
+                              , "weekDay_5", "weekDay_6"
+                              , "weekDay_7"))
 
